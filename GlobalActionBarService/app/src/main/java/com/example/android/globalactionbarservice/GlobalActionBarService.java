a6d83e04620b0ae281802c676962522d91e6da78
aksinghania <akshtsinghnia@gmail.com>
2024-06-18 16:09:37 +0530

    sync

--- GlobalActionBarService/app/src/main/java/com/example/android/globalactionbarservice/GlobalActionBarService.java
+++ GlobalActionBarService/app/src/main/java/com/example/android/globalactionbarservice/GlobalActionBarService.java
@@ -1,24 +1,13 @@
-// Copyright 2016 Google Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package com.example.android.globalactionbarservice;
 
+import android.Manifest;
 import android.accessibilityservice.AccessibilityService;
 import android.accessibilityservice.GestureDescription;
+import android.content.pm.PackageManager;
 import android.graphics.Path;
 import android.graphics.PixelFormat;
 import android.media.AudioManager;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -27,6 +16,9 @@ import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
 import android.widget.Button;
 import android.widget.FrameLayout;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
@@ -49,9 +41,17 @@ public class GlobalActionBarService extends AccessibilityService {
         LayoutInflater inflater = LayoutInflater.from(this);
         inflater.inflate(R.layout.action_bar, mLayout);
         wm.addView(mLayout, lp);
+        requestRecordPermission();
+        Log.i("onserviceconnects",String.valueOf(hasRecordPermission()));
+    }
+    private void requestRecordPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, 0);
     }
 
-
+    private boolean hasRecordPermission() {
+        return ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+                == PackageManager.PERMISSION_GRANTED;
+    }
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
 
